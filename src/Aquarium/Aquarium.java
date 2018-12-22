6d0f13a04e6c9166082d0ffbd0650ef542b5af57
Esteves Tiago <estti379@gmail.com>
2018-12-22 19:03:28 +0100

    UDPClient POSITION_REQUEST send
    -POSITION_REQUEST sending protocol finished
    -ASSOCIATION_REQUEST: added client ID to message sent
    -Added Backbone of messageHandler
    -General correction of spelling mistakes

--- src/Aquarium/Aquarium.java
+++ src/Aquarium/Aquarium.java
@@ -130,7 +130,7 @@ public class Aquarium extends JPanel{
 
                 MobileItem m_item = (MobileItem) item;
                 m_item.move(random_point);
-                //queueBuffer(item);
+                this.aquarium_client.sendFish(item);
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
